caff79350178a38f56dbd83e8df8362a568b0775
Rodrigo Okido <rodrigookido@gmail.com>
2021-03-29 15:21:42 -0300

    Initial Commit

---
+++ Noite_dos_Museus/Noite_dos_MuseusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noite_dos_MuseusApp.swift
+//  Noite_dos_Museus
+//
+//  Created by Rodrigo Okido on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Noite_dos_MuseusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
