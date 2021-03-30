3ae3e87686edc171cfc1b64561937adad5f5a282
illuminaty <naty.brocca@gmail.com>
2021-03-30 14:26:04 -0300

    create a swiftUIView for page Info

---
+++ Noite_dos_Museus/View/EventsInformation.swift
@@ -0,0 +1,22 @@
+//
+//  EventsInformation.swift
+//  Noite_dos_Museus
+//
+//  Created by Natália Brocca dos Santos on 29/03/21.
+//
+
+import SwiftUI
+
+struct EventsInformation: View {
+    var body: some View {
+        NavigationView {
+            
+        }
+    }
+}
+
+struct EventsInformation_Previews: PreviewProvider {
+    static var previews: some View {
+        EventsInformation()
+    }
+}
