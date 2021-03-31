9fc04237d7a285b226b2ea9e0527912963d306d8
juAlberti <julia.maia@acad.pucrs.br>
2021-03-31 18:58:34 -0300

    adding button on splash screen

--- Noite_dos_Museus/View/Onboarding.swift
+++ Noite_dos_Museus/View/Onboarding.swift
@@ -9,78 +9,87 @@ import SwiftUI
 
 struct SplashScreen: View {
     var body: some View {
-        VStack{
-            Spacer(minLength: 320)
-            Image("Logo")
-                .frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            Spacer()
-            HStack{
-                ZStack {
-                    ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)){
-                        Image("Splash screen-2")
-                            .resizable()
-                            .frame(width: 400, height: 300)
-                    }
-                }
-                
-            }
-            .frame(maxHeight: 300)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color("Backcolor"))
-        .edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct Onboarding1: View {
-    var body: some View {
         NavigationView{
             VStack{
+                Spacer(minLength: 320)
+                Image("Logo")
+                    .frame(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                Spacer()
                 HStack{
-                    Image("Tela 1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(maxWidth: 300, maxHeight: 100)
-                    Spacer()
+                    ZStack {
+                        ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)){
+                            Image("Splash screen-2")
+                                .resizable()
+                                .frame(width: 400, height: 300)
+                        }
+                        NavigationLink(
+                            destination: Onboarding1(),
+                            label: {
+                                Text("Próximo")
+                                    .frame(width: 200, height: 50, alignment:.center)
+                                    .background(Color("event_background"))
+                                    .foregroundColor(.white)
+                                    .cornerRadius(25)
+                            })
+                            .padding(.bottom, 200)
+                    }
                 }
-                Spacer()
-                HStack {
-                    Text("A Noite dos Museus é um evento gratuito que busca voltar os holofotes para os museus de Porto Alegre ")
-                        .padding()
-                        .padding(.trailing, 50)
-                        .foregroundColor(.white)
-                        .font(.system(size:20))
-                        .lineLimit(4)
-                    Spacer()
-                }
-                
-                HStack {
-                    Text("O evento conta com uma programação de shows, exibições e workshops e muito mais")
-                        .foregroundColor(.white)
-                        .font(.system(size:20.0))
-                        .lineLimit(4)
-                        .padding()
-                        .padding(.leading, 70)
-                }
-                
-                Spacer()
-                
-                NavigationLink(
-                    destination: Onboarding2(),
-                    label: {
-                        Text("Próximo")
-                            .frame(width: 280, height: 50, alignment:.center)
-                            .background(Color("Buttoncolor"))
-                            .foregroundColor(.white)
-                            .cornerRadius(25)
-                        
-                    })
-                    .padding(.bottom, 70)
+                .frame(maxHeight: 300)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("Backcolor"))
             .edgesIgnoringSafeArea(.all)
         }
+    }
+}
+
+struct Onboarding1: View {
+    var body: some View {
+        VStack{
+            HStack{
+                Image("Tela 1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: 300, maxHeight: 100)
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Text("A Noite dos Museus é um evento gratuito que busca voltar os holofotes para os museus de Porto Alegre ")
+                    .padding()
+                    .padding(.trailing, 50)
+                    .foregroundColor(.white)
+                    .font(.system(size:20))
+                    .lineLimit(4)
+                Spacer()
+            }
+            
+            HStack {
+                Text("O evento conta com uma programação de shows, exibições e workshops e muito mais")
+                    .foregroundColor(.white)
+                    .font(.system(size:20.0))
+                    .lineLimit(4)
+                    .padding()
+                    .padding(.leading, 70)
+            }
+            
+            Spacer()
+            
+            NavigationLink(
+                destination: Onboarding2(),
+                label: {
+                    Text("Próximo")
+                        .frame(width: 280, height: 50, alignment:.center)
+                        .background(Color("Buttoncolor"))
+                        .foregroundColor(.white)
+                        .cornerRadius(25)
+                    
+                })
+                .padding(.bottom, 70)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("Backcolor"))
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
@@ -115,7 +124,6 @@ struct Onboarding2: View {
                         .aspectRatio(contentMode: .fill)
                         .padding(.leading,-80)
                         .frame(maxWidth: 300, maxHeight: 100)
-                        .border(Color.red, width: 5)
                 NavigationLink(
                     destination: Text("Destination"),
                     label: {
@@ -126,7 +134,7 @@ struct Onboarding2: View {
                             .cornerRadius(25)
 
                     })
-                    .padding(.bottom, 40)
+                    .padding(.bottom, 70)
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -140,8 +148,8 @@ struct Onboarding2: View {
 
 struct Onboarding_Previews: PreviewProvider {
     static var previews: some View {
-        //SplashScreen()
-        Onboarding1()
+        SplashScreen()
+        //Onboarding1()
         //Onboarding2()
             .previewDevice("iPhone 12")
             .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
